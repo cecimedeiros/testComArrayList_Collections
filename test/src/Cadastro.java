1c12564c49573f3f8c1c938cac2e940302f85e19
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2023-04-26 14:35:49 -0300

    Update Cadastro.java

--- test/src/Cadastro.java
+++ test/src/Cadastro.java
@@ -19,6 +19,7 @@ class Cadastro {
         }
     }
 
+    //remove um item pelo seu nome
     public boolean remove(String item) {
         if (listaDeTudo.contains(item)) {
             listaDeTudo.remove(item);
@@ -29,6 +30,7 @@ class Cadastro {
 
     }
 
+    //remove um item pelo seu indice
     public boolean remove(int item){
         if (listaDeTudo.contains(listaDeTudo.get(item))){
             listaDeTudo.remove(listaDeTudo.get(item));
@@ -38,6 +40,7 @@ class Cadastro {
         return true;
     }
 
+    //recebe o nome de um item e retorna se ele está ou não na lista
     public boolean contem(String item){
         if (listaDeTudo.contains(item)){
             return true;
@@ -48,14 +51,17 @@ class Cadastro {
         return false;
     }
 
+    //consulta que número está no indice i
     public String recupera(int i){
         return listaDeTudo.get(i);
     }
 
+    //consulta o indice do item
     public int recuperaIndice(String item){
         return listaDeTudo.indexOf(item);
     }
 
+    //ordena a lista bem bonitinha com a quebra de linha /n
     public String toString(){
         String retorno =  "";
         for (String item : listaDeTudo) {
@@ -64,6 +70,7 @@ class Cadastro {
         return retorno;
     }
 
+    //ordena a lista em ordem alfabética
     public void ordena(){
         Collections.sort(listaDeTudo);
     }
